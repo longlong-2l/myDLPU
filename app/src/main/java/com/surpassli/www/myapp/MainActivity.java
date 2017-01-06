36646e287ec214f5e69beeddcd732d393d7d2d7a
longlong-2l <longlong2l@163.com>
2017-01-06 15:02:33 +0800

    Add DataBinding

--- app/src/main/java/com/surpassli/www/myapp/MainActivity.java
+++ app/src/main/java/com/surpassli/www/myapp/MainActivity.java
@@ -1,23 +1,48 @@
 package com.surpassli.www.myapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.surpassli.www.myapp.databinding.ActivityMainBinding;
+import com.surpassli.www.myapp.ui.EducationFragment;
+import com.surpassli.www.myapp.ui.LifeFragment;
+import com.surpassli.www.myapp.ui.MyFragment;
+import com.surpassli.www.myapp.ui.NoNameFragment;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager index_ViewPager;
     private List<Fragment> fragmentList;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        initView();
+    }
 
+    private void initView() {
+
+        fragmentList = new ArrayList<Fragment>();
+        LifeFragment lifeFragment = new LifeFragment();
+        MyFragment myFragment = new MyFragment();
+        EducationFragment educationFragment = new EducationFragment();
+        NoNameFragment noNameFragment = new NoNameFragment();
+
+        fragmentList.add(lifeFragment);
+        fragmentList.add(myFragment);
+        fragmentList.add(educationFragment);
+        fragmentList.add(noNameFragment);
+
+//        binding.indexViewpager.setAdapter();
     }
 }
--- app/src/main/java/com/surpassli/www/myapp/ui/LifeFragment.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/LifeFragment.java
@@ -1,8 +1,8 @@
 package com.surpassli.www.myapp.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,7 @@ import com.surpassli.www.myapp.R;
 /**
  * Created by dell on 2017/1/6.
  */
-public class LifeFragment extends Fragment{
+public class LifeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
