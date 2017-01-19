cac2e9cc405e34d38507e821d6ea63b68e3eebc5
longlong-2l <longlong2l@163.com>
2017-01-19 13:29:46 +0800

    Preference-->About_us

--- app/src/main/java/com/surpassli/www/myapp/ui/More/About/AboutFragment.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/More/About/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.surpassli.www.myapp.ui.More.About;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -10,14 +11,25 @@ import com.surpassli.www.myapp.R;
  * Created by SurpassLi on 2017/1/19.
  */
 public class AboutFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener{
+    private Preference mAppIntroduce;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.more_fragment_about);
+        initView();
+    }
+
+    private void initView() {
+        mAppIntroduce = findPreference("id_app_intro");
+        mAppIntroduce.setOnPreferenceClickListener(this);
     }
 
     @Override
     public boolean onPreferenceClick(Preference preference) {
+        if(mAppIntroduce == preference){
+//            Intent intent = new Intent(getActivity(),);
+
+        }
 
         return false;
     }
