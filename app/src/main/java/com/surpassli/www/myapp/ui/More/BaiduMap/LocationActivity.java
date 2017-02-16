011bb4b38a13eec9e7eec1ca9408321b69f3a14e
longlong-2l <longlong2l@163.com>
2017-02-16 17:29:50 +0800

    地图位置精确

--- app/src/main/java/com/surpassli/www/myapp/ui/More/BaiduMap/LocationActivity.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/More/BaiduMap/LocationActivity.java
@@ -1,22 +1,27 @@
 package com.surpassli.www.myapp.ui.More.BaiduMap;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.TextureMapView;
+import com.baidu.mapapi.model.LatLng;
 import com.surpassli.www.myapp.R;
 import com.surpassli.www.myapp.ui.Base.BaseToolBarActivity;
 
@@ -30,6 +35,10 @@ public class LocationActivity extends AppCompatActivity {
     //定位相关
     private LocationClient mLocationClient;
     private myLocationListener mLocationListener;
+    private boolean isFirstIn = true;
+    private Context mContext;
+    private double myLatitude;
+    private double myLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,7 @@ public class LocationActivity extends AppCompatActivity {
         //初始化Context全局变量，在setContentView之前调用
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_location);
+        this.mContext = this;
         initView();
         initLocation();
     }
@@ -46,7 +56,21 @@ public class LocationActivity extends AppCompatActivity {
         mLocationListener = new myLocationListener();
         mLocationClient.registerLocationListener(mLocationListener);//Client注册监听器
         //为mLocationClient设置一些必要的配置
-        
+        LocationClientOption Option = new LocationClientOption();
+        Option.setCoorType("bd09ll");//必须为bd09ll，原因暂时不知道
+        Option.setIsNeedAddress(true);//设置可以得到地址，方便用地址做其他操作
+        Option.setOpenGps(true);
+        Option.setScanSpan(1000);//每隔一秒钟进行一次请求
+        mLocationClient.setLocOption(Option);//可以解决定位隔几个街区的问题
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mBaiduMap.setMyLocationEnabled(true);//开启允许
+        if(!mLocationClient.isStarted()) {
+            mLocationClient.start();//开启定位
+        }
     }
 
     @Override
@@ -55,10 +79,17 @@ public class LocationActivity extends AppCompatActivity {
         mTextureMapView.onResume();
     }
 
+
     @Override
     protected void onPause() {
         super.onPause();
         mTextureMapView.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
     }
 
     @Override
@@ -91,8 +122,22 @@ public class LocationActivity extends AppCompatActivity {
                     item.setTitle("实时交通（on）");
                 }
                 break;
+            case R.id.id_myLocation:
+                cneterToMyLocation(myLatitude, myLongitude);
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 定位到我的位置
+     * @param myLatitude
+     * @param myLongitude
+     */
+    private void cneterToMyLocation(double myLatitude, double myLongitude) {
+        LatLng latLng = new LatLng(myLatitude, myLongitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);//参数为经纬度，移动地图的位置
+        mBaiduMap.animateMapStatus(msu);
     }
 
     private void initView() {
@@ -113,11 +158,25 @@ public class LocationActivity extends AppCompatActivity {
         mBaiduMap.setMapStatus(msu);
     }
 
-    private class myLocationListener implements BDLocationListener{
+    private class myLocationListener implements BDLocationListener {
 
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
+            MyLocationData data = new MyLocationData.Builder()//
+                    .accuracy(bdLocation.getRadius())//精度
+                    .latitude(bdLocation.getLatitude())//经度
+                    .longitude(bdLocation.getLongitude()).build();//纬度
+            mBaiduMap.setMyLocationData(data);
+            //将定位的位置给予变量，确保其一直是最新的变量
+            myLatitude = bdLocation.getLatitude();
+            myLongitude = bdLocation.getLongitude();
+            if (isFirstIn) {
+                //若是第一次定位
+                cneterToMyLocation(bdLocation.getLatitude(), bdLocation.getLongitude());
+                isFirstIn = false;
 
+                Toast.makeText(mContext,bdLocation.getAddrStr(),Toast.LENGTH_SHORT).show();
+            }
         }
 
         @Override
