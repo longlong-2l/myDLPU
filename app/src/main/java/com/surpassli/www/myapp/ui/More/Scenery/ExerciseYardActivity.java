a9b3244ffc12f580478c6500824d103aa89eca4c
longlong-2l <longlong2l@163.com>
2017-02-17 20:27:40 +0800

    增加了校内景色，校内建筑物

---
+++ app/src/main/java/com/surpassli/www/myapp/ui/More/Scenery/ExerciseYardActivity.java
@@ -0,0 +1,15 @@
+package com.surpassli.www.myapp.ui.More.Scenery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.surpassli.www.myapp.R;
+
+public class ExerciseYardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_yard);
+    }
+}
