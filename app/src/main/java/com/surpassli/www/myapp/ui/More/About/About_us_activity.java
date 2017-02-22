83553e2f0256e59cadf881a7a5ad27a9fc9b2755
longlong-2l <longlong2l@163.com>
2017-02-22 17:24:38 +0800

    啥也没改

--- app/src/main/java/com/surpassli/www/myapp/ui/More/About/About_us_activity.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/More/About/About_us_activity.java
@@ -4,15 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.surpassli.www.myapp.R;
+import com.surpassli.www.myapp.ui.Base.BaseToolBarActivity;
 
 /**
  * Created by SurpassLi on 2017/1/19.
  */
-public class About_us_activity extends Activity{
+public class About_us_activity extends BaseToolBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        initToolBar();
+        setToolbarTitle(getApplicationContext().getString(R.string.app_about));
         getFragmentManager().beginTransaction().replace(R.id.fl_about_framelayout,new AboutFragment()).commit();
     }
 }
