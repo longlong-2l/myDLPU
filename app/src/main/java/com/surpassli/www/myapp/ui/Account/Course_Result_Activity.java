7dda85cb4b9ede5964f9bd18804149905edc8097
longlong-2l <longlong2l@163.com>
2017-06-09 21:42:09 +0800

    完成课程成绩和课程表的Spinner

--- app/src/main/java/com/surpassli/www/myapp/ui/Account/Course_Result_Activity.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/Account/Course_Result_Activity.java
@@ -2,7 +2,13 @@ package com.surpassli.www.myapp.ui.Account;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.surpassli.www.myapp.AppVariables;
 import com.surpassli.www.myapp.R;
@@ -27,13 +33,20 @@ import okhttp3.Response;
  * Created by SurpassLi on 2017/1/17.
  */
 public class Course_Result_Activity extends BaseToolBarActivity {
-//    private RecyclerView mRecycleView;
+
+    private static final String TAG = "Course_Result_Activity";
+    //    private RecyclerView mRecycleView;
     private ListView lv_course;
+    private TextView mEmpty;
+    private Button bt_course_resultSearch;
     private List<Course_Result_bean> mCourse_Result_list;
     private Course_Result_Adapter mCourse_result_adapter;
     private Course_Adapter course_adapter;
     private long mytime;
-    private static final String TAG = "Course_Result_Activity";
+    private String term;
+    private Spinner myCourseResultSpinner;
+    private ArrayAdapter<String> arr_adapter;
+    private static final String[] m = {"2015-2016-1", "2015-2016-2", "2016-2017-1", "2016-2017-2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +54,54 @@ public class Course_Result_Activity extends BaseToolBarActivity {
         setContentView(R.layout.activity_course_result);
         initToolBar();
         setToolbarTitle("课程成绩");
+        MyProgressDialog.showProgressDialog(Course_Result_Activity.this);
+        initView();
+//        getData(term);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MyProgressDialog.showProgressDialog(Course_Result_Activity.this);
-        initView();
     }
 
     private void initView() {
         lv_course = (ListView) findViewById(R.id.lv_course);
+        mEmpty = (TextView) findViewById(R.id.tv_course_result_empty);
+        myCourseResultSpinner = (Spinner) findViewById(R.id.sp_course_result);
+        bt_course_resultSearch = (Button) findViewById(R.id.bt_course_resultSearch);
 //        mRecycleView = (RecyclerView) findViewById(R.id.rv_course_recycleView);
+        //适配器
+        arr_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, m);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        myCourseResultSpinner.setAdapter(arr_adapter);
+        myCourseResultSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                term = m[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        bt_course_resultSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(term);
+            }
+        });
+    }
+
+    private void getData(String term) {
         mCourse_Result_list = new ArrayList<Course_Result_bean>();
         mytime = System.currentTimeMillis() / 1000;//获取系统时间的10位的时间戳
         String timestamp = String.valueOf(mytime + AppVariables.time_cha);
         AppVariables.sign = MD5.getMd5(AppVariables.key + AppVariables.token + timestamp);
-        Log.i(TAG, "initView: "+"userId="+AppVariables.userId+"==token="+AppVariables.token+"==&sign=" + AppVariables.sign +"==timestamp="+timestamp);
-        HttpUtil.sendGetOkHttp_header_sk(AppApi.MY_COURSE + "userId=" + AppVariables.userId + "&sign=" + AppVariables.sign + "&timestamp=" + timestamp,"2015-2016-2", new okhttp3.Callback() {
+        Log.i(TAG, "initView: " + "userId=" + AppVariables.userId + "==token=" + AppVariables.token + "==&sign=" + AppVariables.sign + "==timestamp=" + timestamp);
+        HttpUtil.sendGetOkHttp_header_sk(AppApi.MY_COURSE + "userId=" + AppVariables.userId + "&sign=" + AppVariables.sign + "&timestamp=" + timestamp, term, new okhttp3.Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 MyProgressDialog.closeDialog();
@@ -74,14 +117,21 @@ public class Course_Result_Activity extends BaseToolBarActivity {
                     @Override
                     public void run() {
 //                        mCourse_result_adapter = new Course_Result_Adapter(Course_Result_Activity.this, mCourse_Result_list);
-                         //设置RecycleView 以ListView的方式显示
+                        //设置RecycleView 以ListView的方式显示
 //                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(Course_Result_Activity.this, LinearLayoutManager.VERTICAL, false);
 //                        mRecycleView.setLayoutManager(layoutManager);
 //                        mRecycleView.addItemDecoration(new DividerItemDecoration(Course_Result_Activity.this, DividerItemDecoration.VERTICAL_LIST));//设置分割线
 //                        mRecycleView.setAdapter(mCourse_result_adapter);
 //                        mRecycleView.setItemAnimator(new DefaultItemAnimator());//设置动画效果为默认动画
-                        course_adapter = new Course_Adapter(Course_Result_Activity.this, mCourse_Result_list);
-                        lv_course.setAdapter(course_adapter);
+                        if (mCourse_Result_list == null || mCourse_Result_list.isEmpty()) {
+                            mEmpty.setVisibility(View.VISIBLE);
+                            lv_course.setVisibility(View.GONE);
+                        } else {
+                            mEmpty.setVisibility(View.GONE);
+                            lv_course.setVisibility(View.VISIBLE);
+                            course_adapter = new Course_Adapter(Course_Result_Activity.this, mCourse_Result_list);
+                            lv_course.setAdapter(course_adapter);
+                        }
                         MyProgressDialog.closeDialog();
                     }
                 });
--- app/src/main/java/com/surpassli/www/myapp/ui/Account/Course_Table_Activity.java
+++ app/src/main/java/com/surpassli/www/myapp/ui/Account/Course_Table_Activity.java
@@ -2,12 +2,17 @@ package com.surpassli.www.myapp.ui.Account;
 
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.surpassli.www.myapp.AppVariables;
@@ -32,13 +37,18 @@ import okhttp3.Response;
  * Created by SurpassLi on 2017/1/22.
  */
 public class Course_Table_Activity extends BaseToolBarActivity {
+
+    private static final String TAG = "Course_Table_Activity";
     private ArrayList<Course_Table> course_table_been_list;
-    private EditText et_course_table;
     private RecyclerView rv_course_table;
     private TextView mEmpty;
     private Button bt_search;
+//    private Button bt_choice;
+    private String term;
     private Course_table_recyclerview course_table_recyclerview;
-    private static final String TAG = "Course_Table_Activity";
+    private Spinner myCourseTableSpinner;
+    private ArrayAdapter<String> arr_adapter;
+    private static final String[] m={"2015-2016-1","2015-2016-2","2016-2017-1","2016-2017-2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,21 +59,45 @@ public class Course_Table_Activity extends BaseToolBarActivity {
         initView();
     }
     public void initView(){
-        et_course_table = (EditText) findViewById(R.id.et_course_table);
         rv_course_table = (RecyclerView) findViewById(R.id.rv_course_table);
+        myCourseTableSpinner = (Spinner) findViewById(R.id.sp_course_table);
         mEmpty = (TextView) findViewById(R.id.tv_course_empty);
         bt_search = (Button) findViewById(R.id.bt_Search);
+//        bt_choice = (Button) findViewById(R.id.bt_choice);
+        //适配器
+        arr_adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, m);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        myCourseTableSpinner.setAdapter(arr_adapter);
+        myCourseTableSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                term = m[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        /*bt_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(bt_choice);
+            }
+        });*/
         bt_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getData();
+                getData(term);
             }
         });
     }
 
-    public void getData() {
+    public void getData(final String term) {
         MyProgressDialog.showProgressDialog(Course_Table_Activity.this);
-        final String term = et_course_table.getText().toString();
         course_table_been_list = new ArrayList<Course_Table>();
         long myTime = System.currentTimeMillis()/1000;
         String timesTamp = String.valueOf(myTime + AppVariables.time_cha);
@@ -102,6 +136,7 @@ public class Course_Table_Activity extends BaseToolBarActivity {
                             rv_course_table.setVisibility(View.GONE);
                         } else {
                             mEmpty.setVisibility(View.GONE);
+                            rv_course_table.setVisibility(View.VISIBLE);
                             rv_course_table.setAdapter(course_table_recyclerview);
                             RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(Course_Table_Activity.this, LinearLayoutManager.VERTICAL, false);
                             rv_course_table.setLayoutManager(layoutManager);
@@ -112,4 +147,21 @@ public class Course_Table_Activity extends BaseToolBarActivity {
             }
         });
     }
+
+    /*private void showPopupMenu(View view){
+        //指定pop显示的位置
+        final PopupMenu popupMenu = new PopupMenu(this,view);
+        //menu布局
+        popupMenu.getMenuInflater().inflate(R.menu.menu_course_table,popupMenu.getMenu());
+        //popmenu的点击事件
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                term = item.getTitle().toString();
+                bt_choice.setText(term);
+                return false;
+            }
+        });
+        popupMenu.show();
+    }*/
 }
